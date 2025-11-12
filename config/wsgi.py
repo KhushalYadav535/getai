bb2bcc76adce13e959c251bf96261446e2c43e44
Khushal Yadav <khushalyadav535@gmail.com>
2025-11-12 14:46:31 +0530

    static: serve static and media via WhiteNoise in wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# Serve static files via WhiteNoise
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
